75dc085c1c57d0049c2d0e8fd8b6938f4246d89e
Stephen Parente <sparente@91ferns.com>
2015-09-27 00:35:16 -0400

    Nightly

--- template-parts/content-seed.php
+++ template-parts/content-seed.php
@@ -80,6 +80,126 @@
       <center>
         <a href="btn-center"><img src="<?php ashley_limitless_image('button-sign-up.png'); ?>" alt="Sign Up"></a>
       </center>
+
+      <div class="box clearfix">
+
+        <img src="<?php ashley_limitless_image('testimonial-sarah-d.png'); ?>" class="alignright" alt="Sign Up">
+
+        <p>“I am so glad I participated in Limitless Career Lab, because I was lost on my own – nothing I did was working for me.  Not only was I not getting the job, I was not even getting the interview! (And I thought my resume and cover letter were spotless, so I couldn't imagine why I wasn't getting any calls.) I knew the way I was headed in my career no longer suited me and my needs, but I didn't know how to re-route my journey without losing precious time.</p>
+
+        <p>After working with Ashley, I was able to learn new skills – or re-vamp existing ones – that made me feel confident in my resume and experience.  Ashley helped me get clear about where my talents would be most effective, and map out a variety of paths to get me to the most desired result. This led to SEVERAL interviews (and a few offers!) in the first month. </p>
+
+        <p>Ashley is so passionate about what she does and I really felt like she genuinely wanted to help me. I am so excited for future participants!”</p>
+
+        <div class="author">
+  				<p>
+            <big>Sarah D.</big><br>
+          Philadelphia, PA</p>
+        </div>
+
+      </div>
+
+    </div>
+  </div> <!-- /.container -->
+</section>
+
+<section class="ashley-section with-ashley">
+  <div class="container">
+    <div class="inner-ashley">
+      <p class="lead">
+        I created the <big class="ashley">Limitless Career Lab</big> to help you go through a process that took me YEARS to figure out on my own, in a matter of weeks.
+      </p>
+
+      <p>
+        <img src="<?php ashley_limitless_image('text-limitless-career-is-more.png'); ?>" alt="Sign Up">
+      </p>
+
+      <p>It’s about being a parent who can provide for your family, or a career person who makes positive change in the world by playing BIG (and inspiring others to do the same).</p>
+
+      <p>It’s about becoming the <u>successful, empowered, confident person</u> you know you were meant to be. </p>
+
+      <p>Here’s the deal: confidence comes from knowledge. </p>
+
+      <p><strong>Every day I see highly competent people lose confidence in themselves because they don’t know how to get job offers.</strong></p>
+
+      <p>I’m here to remedy that. </p>
+
+    </div>
+  </div>
+</section>
+
+<section class="program-description-section">
+
+  <div class="container">
+
+    <div class="program-description-inner">
+
+      <center>
+        <img src="<?php ashley_limitless_image('bird.png'); ?>" alt="">
+      </center>
+
+      <h4><span class="ashley">Limitless Career Lab</span> takes the guesswork OUT of the job hunt, and helps you:</h4>
+
+      <ol>
+    		<li>Hone in on your best fitting job options.</li>
+    		<li>Write effective resumes and cover letters.</li>
+    		<li>Learn to master networking (so you can get more job offers).</li>
+    		<li>Negotiate a strong new salary.</li>
+    		<li>Build confidence and empowerment.</li>
+      </ol>
+
+      <p>These are skills that last a lifetime.</p>
+
+      <h4 class="ashley">The Program Includes:</h4>
+
+      <ul>
+    		<li><strong>8 powerful video training modules</strong> (released weekly)</li>
+    		<li><strong>Weekly exercises</strong> that correspond to each module</li>
+    		<li><strong>3 social media tutorials</strong> to help you leverage the power of 	LinkedIn and Twitter</li>
+    		<li><strong>4 Group Coaching Q&amp;A calls,</strong> where I’ll answer your questions LIVE on the line</li>
+    		<li><strong>30+ page workbook</strong> with all of your exercises and worksheets in one place</li>
+    		<li>Access to a <strong>private Facebook group</strong> where you can network with other participants and stay in touch with Ashley</li>
+    		<li><strong>Unlimited access to your course materials</strong> in a password-	protected membership site (expires one year from your purchase date)</li>
+      </ul>
+
+      <p>You get <u>instant access</u> to this 8 week program as you sign up below.</p>
+
+      <p class="lead">Your 8-week program begins as soon as you click the button below.</p>
+
+      <center>
+        <a href="#">
+          <img src="<?php ashley_limitless_image('button-sign-up.png'); ?>" alt="Sign up">
+        </a>
+      </center>
+
+
+    </div> <!-- /.program-description -->
+
+  </div> <!-- /.container -->
+
+</section>
+
+
+<section class="testimonial-section">
+  <div class="container">
+    <div class="testimonial-inner">
+
+      <div class="box clearfix">
+
+        <img src="<?php ashley_limitless_image('testimonial-leah.png'); ?>" class="alignright" alt="Sign Up">
+
+        <p>“Before working with Ashley, I was confused, unable to commit to a decision, and un-focused. After working with Ashley, I was clear on what I wanted (and deserved) and clear on how to make my goals happen with attainable actions and focus.</p>
+
+        <p>I'm so glad I completed the Limitless Career Lab because without Ashley I would not be even close to the clarity and focus I have now. Ash has helped me find my place in the world and most importantly, she upgraded my beliefs about myself. Her energy and passion for servicing others is inspiring. She has truly changed my life.”</p>
+
+        <div class="author">
+  				<p>
+            <big class="felthat">Leah N.</big><br>
+          Lancaster, PA</p>
+        </div>
+
+      </div>
+
     </div>
   </div> <!-- /.container -->
 </section>
